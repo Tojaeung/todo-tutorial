e31511ba017a4d96597c81e3e496c1dc68758652
Tojaeung <kimwoon11@naver.com>
2022-10-10 16:41:41 +0900

    FE: Next.js에 mui적용

--- todo-frontend/src/pages/index.tsx
+++ todo-frontend/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Layout from 'components/Layout';
+import TodoList from 'components/TodoList';
 
 const Home: NextPage = () => {
   return (
